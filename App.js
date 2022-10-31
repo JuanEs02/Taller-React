2e68cd01ba3928f10ba4c760edf93774c3bdde21
JuanEs02 <114095229+JuanEs02@users.noreply.github.com>
2022-10-31 15:44:18 -0500

    Intento de Arreglo App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Variable from './components/Variable';
+
+function App() {
+  return (
+    <div className="App">
+      <Variable/>
+    </div>
+  );
+}
+
+export default App;
