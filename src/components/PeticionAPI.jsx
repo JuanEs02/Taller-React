e9f100f0aa1c247947a220e77b98145df2de5bf3
JuanEs02 <114095229+JuanEs02@users.noreply.github.com>
2022-11-01 23:03:01 -0500

    Creacion de la Card Terminada

--- src/components/PeticionAPI.jsx
+++ src/components/PeticionAPI.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
 const PeticionAPI = () => {
-    
+
     const [personajes, setPersonajes] = React.useState([])
     const [paginacion, setPaginacion] = React.useState(1)
 
     const obtenerPersonajes = async () => {
         try {
-            const res = await fetch(`https://api.disneyapi.dev/characters/?page=${paginacion}`)
-            const {data} = await res.json()
+            const res = await fetch(`https://api.disneyapi.dev/characters?page=${paginacion}`)
+            const { data } = await res.json()
             setPersonajes(data)
         } catch (error) {
             console.log(error)
         }
     }
-    const siguiente = async () =>{
+    const siguiente = async () => {
         await setPaginacion(paginacion + 1)
         obtenerPersonajes()
     }
-    const atras = async () =>{
+    const atras = async () => {
         await setPaginacion(paginacion - 1)
         obtenerPersonajes()
     }
@@ -26,13 +26,19 @@ const PeticionAPI = () => {
         <div>
             <h1>PETICIÓN AL API PERSONAJES DE DISNEY+</h1>
             <button onClick={obtenerPersonajes}> Traer Personajes </button>
-            <button onClick={siguiente}>Siguiente</button>
-            <button onClick={atras}> Atrás</button>
+            <button onClick={siguiente}> Siguiente </button>
+            <button onClick={atras}> Atrás </button>
             {
                 personajes.map((resultado) => (
-                    <div key ={resultado._id}>
-                        <h4>{resultado._id}.{resultado.name}</h4>
-                        <img src={resultado.imageUrl} alt={resultado.name}/>
+                    <div className="row">
+                        <div className="column">
+                            <div key={resultado._id} className="card">
+                                <div className="container">
+                                    <h4>{resultado._id}{". "}{resultado.name}</h4>
+                                    <img src={resultado.imageUrl} alt={resultado.name} />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 )
                 )
